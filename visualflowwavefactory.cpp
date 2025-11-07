b2b19094427c450cd61019d674ab8a7822284c25
Greedysky <greedysky@163.com>
2025-11-07 15:16:40 +0800

    feat: Support for qmmp 0.x Qt4

--- visualflowwavefactory.cpp
+++ visualflowwavefactory.cpp
@@ -39,3 +39,8 @@ QString VisualFlowWaveFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(flowwave, VisualFlowWaveFactory)
+#endif
